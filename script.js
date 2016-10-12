6a3a6ef70c9f5312d9c3c2a36edc9184f9d008ce
Kevin Chau <kchau.jobs@gmail.com>
2016-10-12 12:50:25 -0700

    all is working except adding decimals

--- script.js
+++ script.js
@@ -15,18 +15,42 @@ function getClicked(){
     //console.log("In getClicked");
     var val = $(this).text();
     console.log("button clicked: ", val);
-    my_calculator.addItem(val);
+    switch(val){
+        case 'C':
+            console.log("All clearing!");
+            my_calculator.allClear();
+            break;
+        case 'CE':
+            console.log("Clearing Entry!");
+            my_calculator.clear();
+            break;
+        default:
+            my_calculator.addItem(val);
+    }
 }
 
 function displayStuff(type, value, item){
-    console.log("In Display stuff");
-    if(type === "itemAdded"){
-        $("#numDisplayArea").text(value);
-    }else if(type === "calculated"){
-        $("#operatorDisplayArea").text(value);
-    }else if(type === "error"){
-        $("#display").text(value);
-    }else{
-        debugger;
+    console.log("In Display stuff. Type: ",type," value: ",value);
+    // if(type === "itemAdded"){
+    //     $("#numDisplayArea").text(value);
+    // }else if(type === "calculated"){
+    //     $("#operatorDisplayArea").text("="+value);
+    // }else if(type === "error"){
+    //     $("#display").text(value);
+    // }else{
+    //     $("#numDisplayArea").text("");
+    //     $("#operatorDisplayArea").text("");
+    // }
+    //TODO testing switch statments
+    switch(type){
+        case 'C':
+            $("#numDisplayArea, #calculatedDisplayArea").text("");
+            break;
+        case 'itemAdded':
+            $("#numDisplayArea").text(value);
+            break;
+        case 'calculated':
+            $("#calculatedDisplayArea").text("=" + value);
+            break;
     }
 }
